f8bab75bd7fba8f851c90876fa91a7995c89014c
Alex Nordin <alexnordin.dev@outlook.com>
2022-12-19 00:54:14 +0100

    Added more comment

--- uppgift6/script.js
+++ uppgift6/script.js
@@ -2,12 +2,14 @@
 function myFunction() {
     let input = document.querySelector('input').value;
     let Resultat = document.querySelector('#resultat');
+    /* Om värdet är string visa "Värdet inte är ett nummer." */
     if (isNaN(input)) {
         Resultat.innerHTML = "Värdet inte är ett nummer."
 
-    } else {
+    }
+     /* Annars x tal (input) upphöjt till 2  " */
+    else {
         Resultat.innerHTML = Math.pow(input, 2);
     }
-
 }
 
